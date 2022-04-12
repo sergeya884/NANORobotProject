23516f5086b3da70da887834ffd97256a177080e
cmpolik <samvel.portfolio@gmail.com>
2022-04-12 09:19:57 +0300

    task/04.2 common/camera/camera2.py

--- common/Haara_cascad/test_tank.py
+++ common/Haara_cascad/test_tank.py
@@ -1,18 +1,4 @@
-from SimpleCV import Display, Image
-from time import sleep
+def a (n, **coeff):
 
-myDisplay = Display(resolution=(320,240))
-rasIm = Image("ras.jpg")
-rasIm.listHaarFeatures()
 
-frame=Image("20.jpg")  
-faces=frame.findHaarFeatures('tank.xml')
-if faces:
-    for face in faces:
-        print "Face at: " + str(face.coordinates())
-    faces.show(width=3)
-else:
-    print "NO faces detected."
-    frame.save(myDisplay)
-    
-   
+
